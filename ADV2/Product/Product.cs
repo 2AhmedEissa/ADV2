e38897852f4a201e66c543356d4bf4d9085a18c7
2AhmedEissa <ahmadoeissa@gmail.com>
2026-09-10 17:47:32 +0300

    Add project files.

---
+++ ADV2/Product/Product.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ADV2
+{
+    internal class Product
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Category { get; set; } // "Electronics", "Clothing", "Food", "Books" 
+        public double Price { get; set; }
+        public int Stock { get; set; }
+
+    }
+}
---
+++ ADV2/Program.cs
@@ -0,0 +1,64 @@
+﻿using ADV2;
+using System.ComponentModel;
+using System.Data.Common;
+using System.Diagnostics.CodeAnalysis;
+
+internal class Program
+{
+    static List<Product> catalog = new()
+    {
+        new Product { Id=1, Name="Laptop", Category="Electronics", Price=1200, Stock=10 },
+        new Product { Id=2, Name="Phone", Category="Electronics", Price=800, Stock=25 },
+        new Product { Id=3, Name="T-Shirt", Category="Clothing", Price=30, Stock=100 },
+        new Product { Id=4, Name="Jeans", Category="Clothing", Price=60, Stock=50 },
+        new Product { Id=5, Name="Chocolate", Category="Food", Price=5, Stock=200 },
+        new Product { Id=6, Name="Coffee Beans", Category="Food", Price=15, Stock=80 },
+        new Product { Id=7, Name="C# Book", Category="Books", Price=45, Stock=30 },
+        new Product { Id=8, Name="Novel", Category="Books", Price=20, Stock=60 },
+        new Product { Id=9, Name="Headphones", Category="Electronics", Price=150, Stock=40 },
+        new Product { Id=10, Name="Jacket", Category="Clothing", Price=120, Stock=15 }
+
+
+    };
+
+    delegate int MathOperation(int x, int y);
+    public class calc
+    {
+        public static int Add(int a, int b)
+        {
+            return a + b;
+        }
+        public static int Subtract(int a, int b)
+        {
+            return a - b;
+        }
+
+
+    }
+
+    static int calculate(int a, int b, MathOperation operation)
+    {
+        return operation(a, b);
+    }
+
+    #region Task01
+
+
+   static List<Product> SearchProducts(List<Product> products, Func<Product, bool> filter)
+    {
+        
+    }
+
+    #endregion
+    static void Main(string[] args)
+    {
+        //int a = 10, b = 5;
+
+        //int result = calculate(a, b, calc.Add);
+        //int result2 = calculate(a, b, calc.Subtract);
+
+        //Console.WriteLine(result);
+        //Console.WriteLine(result2);
+        Console.WriteLine(catalog[1].Id);
+    }
+}
