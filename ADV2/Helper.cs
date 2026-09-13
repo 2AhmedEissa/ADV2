91bb160bfb336f6f6233797627ae141c8231ea48
2AhmedEissa <ahmadoeissa@gmail.com>
2026-09-13 07:32:15 +0300

    Task2.1

---
+++ ADV2/Helper.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ADV2
+{
+    internal class Helper
+    {
+
+        public static void PrintList(string listName, List<Product> products)
+        {
+            Console.WriteLine(listName);
+            foreach (Product product in products)
+            {
+                Console.WriteLine($"{product.Name} - ${product.Price} (Stock: {product.Stock})");
+            }
+        }
+
+    }
+}
--- ADV2/Program.cs
+++ ADV2/Program.cs
@@ -2,6 +2,7 @@
 using System.ComponentModel;
 using System.Data.Common;
 using System.Diagnostics.CodeAnalysis;
+using System.Security.Cryptography.X509Certificates;
 
 internal class Program
 {
@@ -20,45 +21,156 @@ internal class Program
 
 
     };
+    #region Delegate as param
+    /*
+    public delegate bool FilterDelegate(int number);
+    public delegate int TransformDelegate(int number);
 
-    delegate int MathOperation(int x, int y);
-    public class calc
+    internal class NumberProcessor
     {
-        public static int Add(int a, int b)
+        public static List<int> Filter(List<int> numbers, FilterDelegate filter)
         {
-            return a + b;
+            List<int> result = new List<int>();
+            foreach (int number in numbers)
+            {
+                if (filter(number))
+                {
+                    result.Add(number);
+                }
+            }
+            return result;
         }
-        public static int Subtract(int a, int b)
+        public static List<int> Transform(List<int> numbers, TransformDelegate transform)
         {
-            return a - b;
+            List<int> result = new();
+            foreach (int number in numbers)
+            {
+                result.Add(transform(number));
+            }
+
+            return result;
+        }
+        public static bool IsEven(int n)
+        {
+            return n % 2 == 0;
+        }
+        public static bool IsOdd(int n)
+        {
+            return n % 2 != 0;
+        }
+        public static bool IsGreaterThan10(int n)
+        {
+            return n > 10;
         }
 
+        public static int Double(int n)
+        {
+            return 2 * n;
+        }
+        public static int Square(int n)
+        {
+            return n * n;
+        }
 
     }
+    */
+    #endregion
 
-    static int calculate(int a, int b, MathOperation operation)
+    #region Multicast Delegates
+
+    delegate void NotificationDelegate(string message);
+
+    internal class NotificationService
     {
-        return operation(a, b);
+        public static void SendEmail(string message)
+        {
+            Console.WriteLine($"Email: {message}");
+        }
+        public static void SendSMS(string message)
+        {
+            Console.WriteLine($"SMS: {message}");
+        }
+        public static void SendPush(string message)
+        {
+            Console.WriteLine($"Push: {message}");
+        }
+        public static void LogToConsole(string message)
+        {
+            Console.WriteLine($"LOG: {message}");
+        }
     }
+
+
+
+    #endregion
+
 
     #region Task01
 
 
-   static List<Product> SearchProducts(List<Product> products, Func<Product, bool> filter)
+    static List<Product> SearchProducts(List<Product> products, Func<Product, bool> filter)
     {
-        
+        List<Product> Result = new();
+        foreach (Product product in products)
+        {
+            if (filter(product))
+            {
+                Result.Add(product);
+            }
+        }
+        return Result;
     }
 
     #endregion
+
+    #region Task 2.1
+
+    public static void PrintReport(List<Product> products, Action<Product> action)
+    {
+        foreach (Product product in products)
+        {
+            action(product);
+        }
+    }
+
+
+    #endregion
+
+
+
     static void Main(string[] args)
     {
-        //int a = 10, b = 5;
+        #region Task01
 
-        //int result = calculate(a, b, calc.Add);
-        //int result2 = calculate(a, b, calc.Subtract);
+        var Electronics = SearchProducts(catalog, p => p.Category == "Electronics");
+        Helper.PrintList("-- Electronics --\n", Electronics);
 
-        //Console.WriteLine(result);
-        //Console.WriteLine(result2);
-        Console.WriteLine(catalog[1].Id);
+        var CheapProducts = SearchProducts(catalog, p => p.Price < 50);
+        Helper.PrintList("\n-- Under 50$ --\n", CheapProducts);
+
+        var InStock = SearchProducts(catalog, p => p.Stock > 0);
+        Helper.PrintList("\n-- InStock --\n", InStock);
+
+        var CheapClothing = SearchProducts(catalog, p => p.Price < 100 && p.Category == "Clothing");
+        Helper.PrintList("\n-- Clothes under 100$ --\n", CheapClothing);
+
+        #endregion
+
+
+        #region Task 2.1
+
+        Console.WriteLine("\n--- Short Report ---\n");
+
+        PrintReport(catalog, p => Console.WriteLine($"{p.Name} - ${p.Price}"));
+
+
+        Console.WriteLine("\n--- Detailed Report ---\n");
+
+        PrintReport(catalog, p => Console.WriteLine($"[{p.Category}] {p.Name} | Price: ${p.Price} | Stock: {p.Stock}"));
+
+
+
+        #endregion
+
     }
 }
